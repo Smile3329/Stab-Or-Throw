1f70de6637402b7aa934af524d9240a3583fc567
Smile3329 <smile3329glhf@gmail.com>
2023-12-29 12:34:54 +0600

    added slider

--- MelonJamProject/Assets/Scripts/Health/HealthController.cs
+++ MelonJamProject/Assets/Scripts/Health/HealthController.cs
@@ -1,20 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthController : MonoBehaviour
 {
     public float health = 10;
+    public Slider slider;
     private GameObject script;
 
     public void InitScript(GameObject script) {
         this.script = script;
+        if (slider != null) {
+            slider.maxValue = health;
+        }
     }
 
     void Update()
     {
         if (health <= 0) {
             script.SendMessage("Die");
+        }
+        if (slider != null) {
+            slider.value = Mathf.Lerp(slider.value, health, Time.deltaTime*4);
         }
     }
 
